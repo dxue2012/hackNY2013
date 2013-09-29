7530e4f64c92366b4bf3eeb6ab99d4cca9350f7f
Amitav Paul <paul.amitav@gmail.com>
2013-09-29 02:22:49 -0400

    parse JSON

--- bitly.php
+++ bitly.php
@@ -9,13 +9,13 @@ function getnews($nofarticle) {
 	$data = json_decode($response, TRUE);
 	foreach($data['data']['values'] as $item) {
 		print $item."<br>";
-		$info_api = 'https://api-ssl.bitly.com/v3/link/info?access_token='.$appkey.'&link='.urlencode($item);
+		echo $info_api = 'https://api-ssl.bitly.com/v3/link/info?access_token='.$appkey.'&link='.urlencode($item);
 		$i = file_get_contents($info_api);
 		$info = json_decode($i, TRUE);
-		print $info['data']['url']."<br>";
+		print $info['data']['canonical_url']."<br>";
 		print $info['data']['html_title']."<br>";
 		
-		$content_api = 'https://api-ssl.bitly.com/v3/link/content?access_token='.$appkey.'&link='.urlencode($item);
+		echo $content_api = 'https://api-ssl.bitly.com/v3/link/content?access_token='.$appkey.'&link='.urlencode($item);
 		$c = file_get_contents($content_api);
 		$content = json_decode($c, TRUE);
 		print $content['data']['content']."<br>";
