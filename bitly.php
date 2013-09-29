c67a7b482c60a0430b652ff03567529d670aeee5
Amitav Paul <paul.amitav@gmail.com>
2013-09-29 03:12:35 -0400

    new 5

--- bitly.php
+++ bitly.php
@@ -5,29 +5,38 @@ $num_of_articles = $_REQUEST['num_of_articles'];
 function getnews($nofarticle) {
     $appkey = "74d2a5411e162025164e146920413e92b8b65b45";
 	$hv_api = 'https://api-ssl.bitly.com/v3/highvalue?access_token='.$appkey.'&limit='.$nofarticle;
-    $response = file_get_contents($hv_api);
-	$data = json_decode($response, TRUE);
+    $hv = file_get_contents($hv_api);
+	$data = json_decode($hv, TRUE);
+	$response = "{"\"data\" : [ ";
+	
 	foreach($data['data']['values'] as $item) {
-		print $item."<br>";
-		$info_api = 'https://api-ssl.bitly.com/v3/link/info?access_token='.$appkey.'&link='.urlencode($item);
-		$i = file_get_contents($info_api);
-		$info = json_decode($i, TRUE);
-		
-		print $info['data']['canonical_url']."<br>";
-		print $info['data']['html_title']."<br>";
 		
 		$content_api = 'https://api-ssl.bitly.com/v3/link/content?access_token='.$appkey.'&link='.urlencode($item);
 		$c = file_get_contents($content_api);
 		$content = json_decode($c, TRUE);
-		print $content['data']['content']."<br>";
-		
-		echo $info_api;
-		echo $content_api;
-		print_r( $info);
-		print_r( $content);
-		print "<br>_______________________________________________________<br>";
+		if( strcmp( $content['status_text'], 'OK' )  == 0 ){
+				
+			$response = $response."{ \"bitly_link\" : \"".$item."\", ";
+			
+			$info_api = 'https://api-ssl.bitly.com/v3/link/info?access_token='.$appkey.'&link='.urlencode($item);
+			$i = file_get_contents($info_api);
+			$info = json_decode($i, TRUE);
+			
+			//print $item."<br>";
+			//print $info['data']['html_title']."<br>";
+			$response = $response."\"title\" : \"".$info['data']['html_title']."\", ";
+			//print $content['data']['content']."<br>";
+			$response = $response."\"content\" : \"".$content['data']['content']."\"";
+			
+			//echo $info_api;
+			//echo $content_api;
+			//print_r( $info);
+			//print_r( $content);
+			//print "<br>_______________________________________________________<br>";
+			$response = $response." },";
+		}
 	}
-	
+	$response = $response." ] }";
 	return $response;
 }
 
